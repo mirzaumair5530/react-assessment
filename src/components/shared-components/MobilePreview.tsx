d960890a34b70d1ad18e99d03c07f09d4220d4d8
Umair <umair@writesea.com>
2023-12-10 21:49:14 +0500

    removed unused import.

--- src/components/shared-components/MobilePreview.tsx
+++ src/components/shared-components/MobilePreview.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment } from "react";
+import { FC } from "react";
 import { Box, styled, Typography } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import {
